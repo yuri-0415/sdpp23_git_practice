64205c9dd0aa8ed499af1aae4f9ff41f7eef5369
yuri-0415 <eileen0415@naver.com>
2023-10-03 12:28:55 +0900

    Revert "Maliciously insert typo"
    This reverts commit ad2578561962b14791ce480c90be6d4a86835576.

--- hello.py
+++ hello.py
@@ -1,13 +1,12 @@
 import sys
 
 def main(animal):
-    aminal = "aminal"
-    if aminal == "cat":
+    if animal == "cat":
         print("Meow!")
     elif animal == "dog":
         print("Woof!")
     else:
-        print("What does the " + aminal + " say?")
+        print("What does the " + animal + " say?")
 
 if __name__ == "__main__":
     main(sys.argv[1])
